268453b6eb1a559a7e137f1a8d8bbbaa864dae44
JensOstertag <JensOstertag@gmx.de>
2023-01-28 14:04:24 +0100

    ClassLoader

---
+++ framework/src/ClassLoader.class.php
@@ -0,0 +1,72 @@
+<?php
+
+class ClassLoader {
+    private static ?ClassLoader $instance = null;
+    
+    private function __construct() {
+    }
+    
+    /**
+     * Returns the Instance of the ClassLoader
+     * @return ClassLoader
+     */
+    public static function getInstance(): ClassLoader {
+        if(self::$instance === null) {
+            self::$instance = new ClassLoader();
+        }
+        
+        return self::$instance;
+    }
+    
+    /**
+     * Loads a single given File
+     * @param string $absolutePath
+     * @return void
+     */
+    public function load(string $absolutePath): void {
+        require_once($absolutePath);
+        Logger::getLogger("ClassLoader")->debug("Loaded File: " . $absolutePath);
+    }
+    
+    /**
+     * Loads a single given Class
+     * Note that only Classes in Files with the Ending ".class.php" will be loaded
+     * @param string $absolutePath
+     * @return bool
+     */
+    public function loadClass(string $absolutePath): bool {
+        if(str_ends_with($absolutePath, ".class.php")) {
+            $this->load($absolutePath);
+            return true;
+        }
+
+        Logger::getLogger("ClassLoader")->debug("Could not load Class: " . $absolutePath . " (File does not end with '.class.php')");
+        
+        return false;
+    }
+    
+    /**
+     * Loads all Classes in a given Directory and it's Subdirectories (recursively) except those specified in $exceptions
+     * Note that only Classes in Files with the Ending ".class.php" will be loaded
+     * @param string $absolutePath
+     * @param array  $exceptions
+     * @return void
+     */
+    public function loadClasses(string $absolutePath, array $exceptions = array()): void {
+        if(is_dir($absolutePath)) {
+            $files = scandir($absolutePath);
+
+            foreach($files as $file) {
+                if(!(is_dir($absolutePath . (str_ends_with($absolutePath, "/") ? "" : "/") . $file))) {
+                    if(str_ends_with($file, ".class.php") && !(in_array($file, $exceptions))) {
+                        $this->loadClass($absolutePath . (str_ends_with($absolutePath, "/") ? "" : "/") . $file);
+                    }
+                } else if($file !== "." && $file !== "..") {
+                    $this->loadClasses($absolutePath . (str_ends_with($absolutePath, "/") ? "" : "/") . $file, $exceptions);
+                }
+            }
+        } else {
+            Logger::getLogger("ClassLoader")->error("Directory {$absolutePath} does not exist");
+        }
+    }
+}
