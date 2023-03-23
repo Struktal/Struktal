60cd8af5b437170367cd3393eac163c89199acd7
JensOstertag <JensOstertag@gmx.de>
2023-03-23 23:07:16 +0100

    Fix #17

--- framework/src/Router.class.php
+++ framework/src/Router.class.php
@@ -4,7 +4,7 @@ class Router {
     private static array $routes = array();
 
     /**
-     * Adds a Route to the Registry
+     * Register a Route
      * @param string $method HTTP Method
      *                       Multiple Methods can be separated with a Pipe (|) Character, without Spaces or other Symbols
      * @param string $route Route that is expected to get called
@@ -16,6 +16,7 @@ class Router {
      * @return void
      */
     public static function addRoute(string $method = "GET|POST", string $route, string $routeTo, string $name) {
+        // Retrieve Parameters from the Route
         $params = array();
         preg_match_all("/\{([bdfis]:[a-zA-Z0-9]+)\}/", $route, $matches);
         foreach($matches[1] as $match) {
@@ -24,6 +25,7 @@ class Router {
             $params[$paramName] = $paramType;
         }
 
+        // Save the Route in the Routes Array
         $methods = explode("|", $method);
         foreach($methods as $method) {
             self::$routes[$method][$route] = array(
@@ -36,7 +38,7 @@ class Router {
     }
 
     /**
-     * Generates the URI of the requested Route
+     * Generate the URI for a Route
      * @param string $name Name of the Route
      * @param array $params GET Parameters that should be added to the URI
      * @return string Route
@@ -87,67 +89,76 @@ class Router {
     }
 
     /**
-     * Starts the Router
+     * Redirect to the File that is registered for the requested Route
+     * This Method also sets Values in the $_GET Array
+     * If no Route is found or the File does not exist, the 404 Page will be opened
+     * If the required Parameters are not valid, the 400 Page will be opened
      * @return void
      */
     public function startRouter(): void {
         $method = $_SERVER["REQUEST_METHOD"];
         $uri = $_SERVER["REQUEST_URI"];
+
+        // Remove GET Parameters after a Question Mark
+        // GET Parameters are set differently
         $uri = explode("?", $uri)[0];
+        // Remove the Root Directory from the URI
+        // This is required if the Project is not located in the Server's Root Directory
         if(str_starts_with($uri, __SERVER_DIR__)) {
             $uri = substr($uri, strlen(__SERVER_DIR__));
         }
+        // Remove leading and trailing Slashes
         $uri = trim($uri, "/");
 
         $foundRoute = array();
-        $foundRouteDepth = 0;
+        $routeFound = false;
         foreach(self::$routes[$method] as $routeData) {
             $route = $routeData["route"];
             $route = trim($route, "/");
             $regex = "";
             $routeParts = explode("/", $route);
-            if(sizeof($routeParts) >= $foundRouteDepth) {
-                // Create regular Expression for the current Route
-                foreach($routeParts as $part) {
-                    if(preg_match("/\{([bdfis]:[a-zA-Z0-9]+)\}/", $part)) {
-                        // Is Parameter
-                        $part = trim($part, "{}");
-                        $paramType = explode(":", $part)[0];
-                        switch($paramType) {
-                            case "b":
-                                $regex .= "[true|false]\/";
-                                break;
-                            case "d":
-                                $regex .= DateFormatter::technicalDateRegex() . "\/";
-                                break;
-                            case "f":
-                                $regex .= "[\d]+(\.[\d]+)?\/";
-                                break;
-                            case "i":
-                                $regex .= "[\d]+\/";
-                                break;
-                            case "s":
-                                $regex .= ".+\/";
-                                break;
-                        }
-                    } else {
-                        // Is no Parameter
-                        $regex .= $part . "\/";
+            // Loop over all Parts of the Route and create a Regex
+            foreach($routeParts as $part) {
+                if(preg_match("/\{([bdfis]:[a-zA-Z0-9]+)\}/", $part)) {
+                    // The current Route Part is a Parameter
+                    // Add Regex for the corresponding Parameter Type
+                    $part = trim($part, "{}");
+                    $paramType = explode(":", $part)[0];
+                    switch($paramType) {
+                        case "b":
+                            $regex .= "true|false\/";
+                            break;
+                        case "d":
+                            $regex .= DateFormatter::technicalDateRegex() . "\/";
+                            break;
+                        case "f":
+                            $regex .= "[\d]+(\.[\d]+)?\/";
+                            break;
+                        case "i":
+                            $regex .= "[\d]+\/";
+                            break;
+                        case "s":
+                            $regex .= ".+\/";
+                            break;
                     }
+                } else {
+                    // The current Route Part is no Parameter
+                    // Simply add the Part to the Regex
+                    $regex .= $part . "\/";
                 }
-                if(str_ends_with($regex, "\/")) {
-                    $regex = substr($regex, 0, strlen($regex) - 2);
-                }
+            }
+            if(str_ends_with($regex, "\/")) {
+                $regex = substr($regex, 0, strlen($regex) - 2);
+            }
 
-                if(preg_match("#" . $regex . "#", $uri)) {
-                    // The current Route matches the Request
-                    $foundRoute = $routeData;
-                    $foundRouteDepth = sizeof($routeParts);
-                }
+            if(preg_match("#^" . $regex . "$#i", $uri)) {
+                // The current Route matches the Request
+                $foundRoute = $routeData;
+                $routeFound = true;
             }
         }
 
-        if(!(array_key_exists("route", $foundRoute))) {
+        if(!($routeFound)) {
             Comm::redirect(Router::generate("404"));
             http_response_code(404);
             return;
@@ -157,60 +168,30 @@ class Router {
         $route = trim($route, "/");
         $routeTo = $foundRoute["routeTo"];
 
-        // Set GET Parameters
+        // Set the GET Parameters
+        // Loop over all Parts of the Route
         foreach(explode("/", $route) as $key => $part) {
             if(preg_match("/\{([bdfis]:[a-zA-Z0-9]+)\}/", $part)) {
+                // The current Route Part is a Parameter
+                // Retrieve the Parameter Type and Name from the Route Part and the Value from the URI
                 $part = trim($part, "{}");
                 $paramType = explode(":", $part)[0];
                 $paramName = str_replace($paramType . ":", "", $part);
                 $paramValue = explode("/", $uri)[$key];
 
-                switch($paramType) {
-                    case "b":
-                        if(filter_var($paramValue, FILTER_VALIDATE_BOOLEAN)) {
-                            $paramValue = boolval($paramValue);
-                        } else {
-                            Comm::redirect(Router::generate("400"));
-                            http_response_code(400);
-                            return;
-                        }
-                        break;
-                    case "d":
-                        if(DateTime::createFromFormat(Config::$DATETIME_SETTINGS["DATE_TECHNICAL"], $paramValue) !== false) {
-                            $paramValue = strval($paramValue);
-                        } else {
-                            Comm::redirect(Router::generate("400"));
-                            http_response_code(400);
-                            return;
-                        }
-                        break;
-                    case "f":
-                        if(filter_Var($paramValue, FILTER_VALIDATE_FLOAT)) {
-                            $paramValue = floatval($paramValue);
-                        } else {
-                            Comm::redirect(Router::generate("400"));
-                            http_response_code(400);
-                            return;
-                        }
-                        break;
-                    case "i":
-                        if(filter_var($paramValue, FILTER_VALIDATE_INT)) {
-                            $paramValue = intval($paramValue);
-                        } else {
-                            Comm::redirect(Router::generate("400"));
-                            http_response_code(400);
-                            return;
-                        }
-                        break;
-                    case "s":
-                        $paramValue = strval($paramValue);
-                        break;
+                if(self::getParameterFromString($paramValue, $paramType) !== null) {
+                    $paramValue = self::getParameterFromString($paramValue, $paramType);
+                } else {
+                    Comm::redirect(Router::generate("400"));
+                    http_response_code(400);
+                    return;
                 }
 
                 $_GET[$paramName] = urldecode($paramValue);
             }
         }
 
+        // Redirect to the File that is registered for the Route
         if(str_ends_with($routeTo, ".php")) {
             if(file_exists($routeTo)) {
                 include_once($routeTo);
@@ -233,20 +214,55 @@ class Router {
     }
     
     /**
-     * Generates the Import Path for a File in the Static Directory
-     * @param string $file
+     * Generate the Import Path for a File within the Static Directory
+     * @param string $path File Path
      * @return string
      */
-    public static function staticFilePath(string $file): string {
-        return __SERVER_DIR__ . "static/" . trim($file, "/");
+    public static function staticFilePath(string $path): string {
+        return __SERVER_DIR__ . "static/" . trim($path, "/");
     }
 
     /**
-     * Sends the correct Content-Type Header for a given File
-     * @param $path
+     * If Parsing is possible, get the Parameter of the corresponding Type from a String
+     * @param mixed $value Value that should be parsed
+     * @param string $parameter Type of the Parameter (b, d, f, i, s)
+     * @return mixed|null Parsed Parameter or null if Parsing is not possible
+     */
+    private static function getParameterFromString(mixed $value, string $parameter): mixed {
+        switch($parameter) {
+            case "b":
+                if(filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) !== null) {
+                    return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                }
+                break;
+            case "d":
+                if(DateTime::createFromFormat(Config::$DATETIME_SETTINGS["DATE_TECHNICAL"], $value) !== false) {
+                    return strval($value);
+                }
+                break;
+            case "f":
+                if(filter_var($value, FILTER_VALIDATE_FLOAT)) {
+                    return floatval($value);
+                }
+                break;
+            case "i":
+                if(filter_var($value, FILTER_VALIDATE_INT)) {
+                    return intval($value);
+                }
+                break;
+            case "s":
+                return strval($value);
+        }
+
+        return null;
+    }
+
+    /**
+     * Send the correct Content-Type Header for a given File
+     * @param string $file File Name or Path
      * @return void
      */
-    private function sendContentTypeHeader($path): void {
+    private function sendContentTypeHeader(string $file): void {
         $extensions = array(
             "html" => "text/html",
             "css" => "text/css",
@@ -291,7 +307,7 @@ class Router {
         );
 
         foreach($extensions as $extension => $contentType) {
-            if(str_ends_with($path, "." . $extension)) {
+            if(str_ends_with($file, "." . $extension)) {
                 header("Content-Type: $contentType");
                 return;
             }
--- project/htdocs/400.php
+++ project/htdocs/400.php
@@ -1,3 +1,4 @@
 <?php
 
+http_response_code(400);
 Template::loadTemplate("400.php");
