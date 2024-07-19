6bc58ef03b9d58ebdbf5361151eecd4d49a09257
JensOstertag <dev@jens-ostertag.de>
2024-07-19 09:39:46 +0200

    Better parsing for accept-language header

--- project/src/lib/TranslationUtil.class.php
+++ project/src/lib/TranslationUtil.class.php
@@ -3,6 +3,8 @@
 class TranslationUtil {
     const TRANSLATIONS_PATH = __APP_DIR__ . "/project/translations";
 
+    private static string $regex = "/[a-zA-Z]{1,8}(?:-[a-zA-Z]{1,8}){0,2}(?:;q=[0-9]+(?:\.[0-9]+)?)?/";
+
     /**
      * Returns the users preferred locale
      * @return string
@@ -33,6 +35,50 @@ class TranslationUtil {
 
         // Fallback
         return "en_US";
+    }
+
+    /**
+     * Get a sorted list of preferred locales from the Accept-Language header
+     * @return array
+     */
+    public static function getPreferredLocalesFromHeader(): array {
+        $header = $_SERVER["HTTP_ACCEPT_LANGUAGE"] ?? "";
+        $header = trim($header);
+
+        // Match regex against header to extract the language parts
+        preg_match_all(self::$regex, $header, $headerParts);
+        $headerParts = $headerParts[0];
+
+        // Interpret the language parts
+        $preferredLocales = array_map(function(string $part) {
+            if(!$part) {
+                return null;
+            }
+
+            $bits = explode(";", $part);
+            $localeTag = explode("-", $bits[0]);
+            $hasScript = count($localeTag) === 3;
+
+            return [
+                "code" => $bits[0],
+                "priority" => count($bits) > 1 ? floatval(explode("=", $bits[1])[1]) : 1.0,
+                "language" => $localeTag[0],
+                "script" => $hasScript ? $localeTag[1] : null,
+                "region" => $hasScript ? $localeTag[2] : $localeTag[1]
+            ];
+        }, $headerParts);
+
+        // Filter out null values
+        $preferredLocales = array_filter($preferredLocales, function($part) {
+            return $part !== null;
+        });
+
+        // Sort locales by their priority
+        usort($preferredLocales, function($a, $b) {
+            return $b["priority"] <=> $a["priority"];
+        });
+
+        return $preferredLocales;
     }
 
     /**
