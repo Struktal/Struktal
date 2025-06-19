cb52604eb55ad178f803498077ba9bde393dfca4
JensOstertag <dev@jens-ostertag.de>
2025-06-19 17:14:36 +0200

    Fix typo

--- src/pages/auth/recovery-request.php
+++ src/pages/auth/recovery-request.php
@@ -2,7 +2,7 @@
 
 // Check whether the user is already logged in
 if(Auth::isLoggedIn()) {
-    Comm::redirect(Router->enerate("index"));
+    Comm::redirect(Router->generate("index"));
 }
 
 echo Blade->run("auth.recoveryrequest");
