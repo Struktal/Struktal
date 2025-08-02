af2c66041bbbc043acfebcde4a40059f770f8ae4
JensOstertag <dev@jens-ostertag.de>
2025-08-02 10:16:43 +0200

    Highlight active menu item

--- src/templates/components/layout/sidebars/consolelist.blade.php
+++ src/templates/components/layout/sidebars/consolelist.blade.php
@@ -2,26 +2,8 @@
     @component("components.layout.sidebars.consoleitem", [
         "href" => Router->generate("index"),
         "icon" => "components.icons.home",
-        "active" => true
+        "active" => in_array(Router->getCalledRouteName(), [ "console" ])
     ])
         {{ t("Home") }}
     @endcomponent
-
-        @component("components.layout.sidebars.consoleitem", [
-            "href" => Router->generate("index")
-        ])
-            {{ t("Home") }}
-        @endcomponent
-
-        @component("components.layout.sidebars.consoleitem", [
-            "href" => Router->generate("index")
-        ])
-            {{ t("Home") }}
-        @endcomponent
-
-        @component("components.layout.sidebars.consoleitem", [
-            "href" => Router->generate("index")
-        ])
-            {{ t("Home") }}
-        @endcomponent
 </ul>
