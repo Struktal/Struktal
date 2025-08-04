b183953d351e00d14a497f1472dc89a1df60f6b8
JensOstertag <dev@jens-ostertag.de>
2025-08-04 11:34:08 +0200

    Enhance safelist handling

---
+++ src/templates/.safelist/safelist.blade.php
@@ -0,0 +1,10 @@
+{{-- This component is not used anywhere. --}}
+{{-- It is used to ensure that certain classes, e.g. those from TailwindUtil, are always present in the final CSS: --}}
+<div class="
+    w-full h-full md:w-full md:h-full
+    translate-x-full md:translate-x-full
+    flex inline-flex justify-around items-center border rounded
+    text-sm font-bold
+    data-[required]:after:content-['*'] data-[required]:after:text-primary-500
+    placeholder:text-surface-500
+"></div>
