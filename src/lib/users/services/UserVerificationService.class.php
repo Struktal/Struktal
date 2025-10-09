ec3e846ddb7519bdd3a58e9ad70feaf8588ae9eb
JensOstertag <dev@jens-ostertag.de>
2025-10-09 18:36:16 +0200

    Use UserVerificationService

--- src/lib/users/services/UserVerificationService.class.php
+++ src/lib/users/services/UserVerificationService.class.php
@@ -6,7 +6,11 @@ class UserVerificationService {
     public static function verify(User $user): void {
         $user->setEmailVerified(true);
         $user->setOneTimePassword(null);
+        $user->setOneTimePasswordExpiration(null);
+        $user->setUpdated(new \DateTimeImmutable());
         User::dao()->save($user);
+
+        Logger->tag("Email-Verification")->info("The email address \"{$user->getEmail()}\" (User ID \"{$user->getId()}\") has been verified");
     }
 
     public static function generateVerificationLink(int $otpId, string $otp): string {
@@ -14,5 +18,35 @@ class UserVerificationService {
         $otpEncoded = urlencode($otp);
 
         return Router->generate("auth-verify-email", [], true) . "?otpid=" . $otpIdEncoded . "&otp=" . $otpEncoded;
+    }
+
+    public static function verifyOtp(int|string $otpId, string $otp, bool $isUrlEncoded): User {
+        if($isUrlEncoded) {
+            $otpId = base64_decode(urldecode($otpId));
+            $otp = urldecode($otp);
+        }
+
+        // Find the user from the one-time password
+        $user = \app\users\User::dao()->getObject([
+            "id" => $otpId,
+            "emailVerified" => false,
+            new \struktal\ORM\DAOFilter(
+                \struktal\ORM\DAOFilterOperator::NOT_EQUALS,
+                "oneTimePassword",
+                null
+            ),
+            "oneTimePasswordExpiration" => null
+        ]);
+
+        if(!$user instanceof User) {
+            Logger->tag("Email-Verification")->info("Attempted to verify an email, but couldn't find user with otpid \"{$otpId}\"");
+            throw new UserNotFoundException();
+        }
+        if(!password_verify($otp, $user->getOneTimePassword())) {
+            Logger->tag("Email-Verification")->info("Attempted to verify an email, but one-time password does not match");
+            throw new UserNotFoundException();
+        }
+
+        return $user;
     }
 }
--- src/pages/auth/verify-email.php
+++ src/pages/auth/verify-email.php
@@ -22,38 +22,15 @@ try {
     Router->redirect(Router->generate("auth-login"));
 }
 
-$otpId = base64_decode(urldecode($get["otpid"]));
-$otp = urldecode($get["otp"]);
-
 // Find the user from the one-time password
-$user = \app\users\User::dao()->getObject([
-    "id" => $otpId,
-    "emailVerified" => false,
-    new \struktal\ORM\DAOFilter(
-        \struktal\ORM\DAOFilterOperator::NOT_EQUALS,
-        "oneTimePassword",
-        null
-    ),
-    "oneTimePasswordExpiration" => null
-]);
-if(!$user instanceof \app\users\User) {
-    Logger->tag("Email-Verification")->info("Attempted to verify an email, but couldn't find user with otpid \"{$otpId}\"");
-    InfoMessage->error(t("The URL has already been invalidated. Please log in or request a new password recovery email."));
-    Router->redirect(Router->generate("auth-login"));
-}
-if(!password_verify($otp, $user->getOneTimePassword())) {
-    Logger->tag("Email-Verification")->info("Attempted to verify an email, but one-time password does not match");
+try {
+    $user = \app\users\UserVerificationService::verifyOtp($get["otpid"], $get["otp"], true);
+} catch(\app\users\UserNotFoundException $e) {
     InfoMessage->error(t("The URL has already been invalidated. Please log in or request a new password recovery email."));
     Router->redirect(Router->generate("auth-login"));
 }
 
 // Update the user object in the database
-$user->setEmailVerified(true);
-$user->setOneTimePassword(null);
-$user->setOneTimePasswordExpiration(null);
-$user->setUpdated(new DateTimeImmutable());
-\app\users\User::dao()->save($user);
-
-Logger->tag("Email-Verification")->info("The email address \"{$user->getEmail()}\" (User ID \"{$user->getId()}\") has been verified");
+\app\users\UserVerificationService::verify($user);
 
 Router->redirect(Router->generate("auth-verify-email-complete"));
