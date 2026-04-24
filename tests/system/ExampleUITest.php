fb9fe16102fc33d0748c56d6a7ef394b30f541ab
JensOstertag <dev@jens-ostertag.de>
2026-04-24 17:40:16 +0200

    Use new test script

--- tests/system/ExampleUITest.php
+++ tests/system/ExampleUITest.php
@@ -6,7 +6,7 @@ test("Example UI test", function() {
     $browser = Playwright::firefox();
     $page = $browser->newPage();
 
-    $request = $page->goto("http://localhost:3000");
+    $request = $page->goto(Router->generate("index", [], true));
     expect($request)->not()->toBeNull()
         ->and($request->ok())->toBeTrue();
 
